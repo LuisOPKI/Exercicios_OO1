ccb7df6bfc8e05f6ded7c70bc540713574fbd116
Luis_Pochynski <Luis_Pochynski@192.168.1.44>
2026-05-06 14:58:54 -0300

    Classe Banco

--- src/lista5/exercicio1/Banco.java
+++ src/lista5/exercicio1/Banco.java
@@ -9,8 +9,18 @@ public class Banco {
 	public Banco(List<Conta> conta) {
 		this.conta = new ArrayList<Conta>();
 	}
-	public void criarConta() {
+	public void criarConta(int numero, String nome, String cpf) {
+		Conta conta = new Conta(numero, new Cliente(nome, cpf));
+		this.conta.add(conta);
 		
+	
+	
 	}
-		
+//	private boolean verificarNumero(int numero) {
+//		for(Conta conta : this.conta) {
+			
+//			return true;
+//		}
+//	}
 }
+
