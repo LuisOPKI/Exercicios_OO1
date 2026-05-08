32eb5e0389bd2120ccbf8b336d29aa369fd76fb1
Luis_Pochynski <Luis_Pochynski@192.168.1.44>
2026-05-08 14:20:48 -0300

    Exercicio 2

--- src/lista5/exercicio2/Biblioteca.java
+++ src/lista5/exercicio2/Biblioteca.java
@@ -10,11 +10,45 @@ public class Biblioteca {
 		this.livros = new ArrayList<Livro>();
 	}
 	
-	public void emprestar(String titulo, String dataEmprestimo) {
-		for(Livro livro : livros) {
-			if(titulo.equalsIgnoreCase(livro.getTitulo())) {
-				
+	public void emprestarLivro(String titulo, int diaEmprestimo) {
+		Livro livro = this.buscarLivro(titulo);
+		if(livro == null) {
+		System.out.println("O livro não foi encontrado!");
+	}
+		else {
+			if(livro.isDisponibilidade() == true) {
+				livro.setDisponibilidade(false);
+				livro.setDiaEmprestimo(diaEmprestimo);
+				System.out.println("O " + titulo + "está sob seus cuidados!");
+			}
+			else {
+			System.out.println("O " + titulo + "não está disponível para emprestimo");
 			}
 		}
 	}
+	
+	public void devolverLivro(String titulo, int diaEmprestimo) {
+		Livro livro = this.buscarLivro(titulo);
+		if(livro == null) {
+			System.out.println("O livro não foi encontrado!");
+			return;
+		}
+		if(livro.isDisponibilidade() == false) {
+			this.conferirAtraso(diaEmprestimo);
+		}
+		
+	}
+	
+	private Livro buscarLivro(String titulo) {
+		for(Livro livro : livros) {
+			if(livro.getTitulo().equals(titulo)) {
+				return livro;
+			}
+		}
+		return null;
+	}
+	private Livro conferirAtraso(int diaEmprestimo) {
+		
+	}
 }
+
--- src/lista5/exercicio2/Livro.java
+++ src/lista5/exercicio2/Livro.java
@@ -7,7 +7,7 @@ public class Livro {
 	private String editora;
 	private int numMaxPaginas;
 	private boolean disponibilidade;
-	private String dataEmprestimo;
+	private int diaEmprestimo;
 	
 	public Livro(String titulo, String autor, int anoPublicacao, String editora, int numMaxPaginas) {
 		this.titulo = titulo;
@@ -16,23 +16,63 @@ public class Livro {
 		this.editora = editora;
 		this.numMaxPaginas = numMaxPaginas;
 		this.disponibilidade = true;
-		this.dataEmprestimo = null;
+		this.diaEmprestimo = 0;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
 	public String getAutor() {
 		return autor;
 	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
 	public int getAnoPublicacao() {
 		return anoPublicacao;
 	}
+
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
 	public String getEditora() {
 		return editora;
 	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
 	public int getNumMaxPaginas() {
 		return numMaxPaginas;
 	}
-	
+
+	public void setNumMaxPaginas(int numMaxPaginas) {
+		this.numMaxPaginas = numMaxPaginas;
+	}
+
+	public boolean isDisponibilidade() {
+		return disponibilidade;
+	}
+
+	public void setDisponibilidade(boolean disponibilidade) {
+		this.disponibilidade = disponibilidade;
+	}
+
+	public int getDiaEmprestimo() {
+		return diaEmprestimo;
+	}
+
+	public void setDiaEmprestimo(int diaEmprestimo) {
+		this.diaEmprestimo = diaEmprestimo;
+	}
 	
 }
