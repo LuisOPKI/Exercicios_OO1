f5206a28ebabbd78d7e2400eafd2422a6acec795
LuisOPKI <luisotaviopochynski@gmail.com>
2026-06-17 14:36:40 -0300

    Exercicio 3

--- src/lista7/exercicio3/Cliente.java
+++ src/lista7/exercicio3/Cliente.java
@@ -1,36 +1,7 @@
 package lista7.exercicio3;
 
-public abstract class Cliente {
+public class Cliente {
     protected String nome;
-    protected String dataNascimento; // apenas dia do mês
-    protected String telefone;
+    protected String cpf;
     protected Endereco endereco;
-
-    public Cliente(String nome, String dataNascimento, String telefone, Endereco endereco) {
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-        this.endereco = endereco;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    @Override
-    public String toString() {
-        return nome + ", " + dataNascimento + ", " + telefone + ", " + endereco;
-    }
 }
--- src/lista7/exercicio3/SistemaLocacao.java
+++ src/lista7/exercicio3/SistemaLocacao.java
@@ -1,5 +1,32 @@
 package lista7.exercicio3;
 
-public abstract class SistemaLocacao {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaLocacao {
+
+    private List<Cliente> clientes;
+    private List<Filme> filmes;
+
+    public SistemaLocacao() {
+        this.clientes = new ArrayList<Cliente>();
+        this.filmes = new ArrayList<>();
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        this.clientes.add(cliente);
+    }
+
+    public void adicionarFilme(Filme filme) {
+        this.filmes.add(filme);
+    }
+
+    public void alugarFilme(Cliente cliente, Filme filme) {
+        
+    }
+
+    public void devolverFilme(Cliente cliente, Filme filme) {
+        
+    }
+
 }
